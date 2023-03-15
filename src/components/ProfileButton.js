a0fa103b0b43cf3b89ab66e6169c45e97684b0f4
chris92vr <garofolichristian@gmail.com>
2023-03-16 00:35:50 +0100

    Updated Header response

--- src/components/ProfileButton.js
+++ src/components/ProfileButton.js
@@ -25,6 +25,14 @@ const ProfileButton = () => {
 
     response
       .then((res) => {
+        res.header(
+          'Access-Control-Allow-Origin',
+          'https://budget-tracker-frontend-delta.vercel.app'
+        );
+        res.header(
+          'Access-Control-Allow-Headers',
+          'Origin, X-Requested-With, Content-Type, Accept'
+        );
         if (res.status === 200) {
           navigate('/profile');
         } else {
