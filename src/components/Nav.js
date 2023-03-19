ff9b7f6959a1c8eda2d0532b6c053893cbd7a8ad
chris92vr <garofolichristian@gmail.com>
2023-03-19 18:34:34 +0100

    Fixed NAV and home user controll

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -3,7 +3,7 @@ import ProfileButton from './ProfileButton';
 import LogoutButton from './Logoutbutton';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
-
+import { useState, useEffect } from 'react';
 import {
   faHome,
   faSignInAlt,
@@ -13,7 +13,37 @@ import {
 import { isUserLoggedIn } from '../utils';
 
 const Nav = () => {
-  console.log('isUserLoggedIn nav', isUserLoggedIn());
+  const [userLogged, setUserLogged] = useState(false);
+  useEffect(() => {
+    // declare the async data fetching function
+    const fetchData = async () => {
+      // get the data from the api
+      const user = await fetch(
+        'https://budgeet-tracker-api.herokuapp.com/protected',
+        {
+          credentials: 'include',
+          mode: 'cors',
+          method: 'GET',
+          AccessControlAllowOrigin:
+            'https://budget-tracker-frontend-delta.vercel.app',
+        }
+      );
+
+      // convert the data to json
+      const json = await user.json();
+
+      // set state with the result
+
+      setUserLogged(json.username);
+    };
+
+    // call the function
+    const result = fetchData()
+      // make sure to catch any error
+      .catch(console.error);
+    console.log(result);
+  }, []);
+
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <div className="container-fluid">
@@ -38,7 +68,7 @@ const Nav = () => {
                 <FontAwesomeIcon icon={faHome} /> Home
               </Link>
             </li>
-            {isUserLoggedIn ? (
+            {userLogged ? (
               <li className="nav-item">
                 <Link className="nav-link" to="/profile">
                   <ProfileButton />
